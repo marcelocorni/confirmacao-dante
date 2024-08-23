df59ece92803d6d919683bbc3ee3a5aca40c604d
Marcelo Corni Alves <marcelo@desenvolvedor.net>
2024-08-23 16:48:14 -0300

    Dante Confirmation

--- pages/agradecimento.py
+++ pages/agradecimento.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 # Tela de agradecimento e QR Code
 st.title("Obrigado por confirmar sua presença!")
-st.write("Se quiser contribuir com uma cerveja para o desenvolvedor (pai do Dante), use o QR Code PIX abaixo:")
+st.write("Se quiser contribuir com uma cerveja para o desenvolvedor (pai do Dante) ou um docinho para a organizadora (mãe do Dante), use o QR Code PIX abaixo:")
 
 st.image('images/qrcode-pix.png')
--- pages/agradecimento_final.py
+++ pages/agradecimento_final.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 # Tela de agradecimento e QR Code após prazo
 st.title("Não fique chateado(a)!")
-st.write("Mas se ainda assim quiser contribuir com uma cerveja para o desenvolvedor (pai do Dante), use o QR Code PIX ao lado:")
+st.write("Mas se ainda assim quiser contribuir com uma cerveja para o desenvolvedor (pai do Dante) ou um docinho para a organizadora (mãe do Dante), use o QR Code PIX ao lado:")
 
 st.image('images/qrcode-pix.png', width=250)
